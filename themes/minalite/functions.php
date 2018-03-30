e8b5d856e1cf2310ff86e503dd0084e96e6407eb
mikkeljuhl <mikkeljjuhl@gmail.com>
2018-03-30 13:26:12 +0200

    Exclude 'gravel ruter'

--- themes/minalite/functions.php
+++ themes/minalite/functions.php
@@ -211,3 +211,11 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+function exclude_category( $query ) {
+	if ( $query->is_home() && $query->is_main_query() ) {
+		$query->set( 'cat', '-11' );
+	}
+}
+
+add_action( 'pre_get_posts', 'exclude_category' );
